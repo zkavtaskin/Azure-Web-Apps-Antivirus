ae63012fd7459d09814f1279ba7314fb4dba5057
Zan Kavtaskin <zan.kavtaskin@gmail.com>
2017-02-07 22:45:33 +0000

    Updated instruct

--- AVFileReceiver/PowerShell/InstallSymantec.ps1
+++ AVFileReceiver/PowerShell/InstallSymantec.ps1
@@ -1,11 +1,10 @@
-
-# Prerequisites:
+﻿# Prerequisites:
 # 1. Make sure VM agent is installed on the VM
 # 2. Enable port 80 or 443 
-# 2. Install PowerShellGet on the VM
+# 3. Install PowerShellGet on the VM
 #	 go to https://github.com/PowerShell/PowerShellGet#get-powershellget-module-for-powershell-versions-30-and-40 
 #	 and download and install MSI 
-# 3. Enable Active Scripting so that you can authenticate
+# 4. Enable "Active Scripting" so that you can login to Azure
 #	 Go IE -> Internet Options -> Security -> Custom Level -> Scroll to Scripting and Enable "Active Scripting"
 
 param(
@@ -22,10 +21,10 @@ write-host $vm.VM.ProvisionGuestAgent
 
 $Agent = Get-AzureVMAvailableExtension -Publisher Symantec -ExtensionName SymantecEndpointProtection
 
-Set-AzureVMExtension -Publisher Symantec �Version $Agent.Version -ExtensionName SymantecEndpointProtection -VM $vm | Update-AzureVM
+Set-AzureVMExtension -Publisher Symantec –Version $Agent.Version -ExtensionName SymantecEndpointProtection -VM $vm | Update-AzureVM
 
 Restart-Computer
 
-# Final Manual step:
-# On the task bar find Symantec client and review the status, most likely it's not working
-# you will need press Fix-All and after few minutes you will need to reboot your VM again
+# Final manual step:
+# On the taskbar find Symantec client and review the status, most likely it's not working
+# you will need press Fix-All and after few minutes Symantec will ask you to reboot your VM again
